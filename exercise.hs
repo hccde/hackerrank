7181e91bfbb329d096c17801615ee165849496b1
admos_han <hancongcong@zhongan.io>
2019-03-27 13:39:26 +0800

    code

--- exercise.hs
+++ exercise.hs
@@ -1,3 +1,4 @@
+import Data.Maybe as Maybe
 import Data.List as List
 modfiList::[Int]->Int->Int->[Int]
 modfiList list index elm =  pre ++ [elm] ++ behind  where
@@ -5,8 +6,8 @@ modfiList list index elm =  pre ++ [elm] ++ behind  where
             behind = List.drop (index+1) $ list;
 main::IO()
 
-closureFn::Int->Int->Int
-closureFn param = (\x -> x+param+inner) where
+closureFn::Int->Int
+closureFn  = (\x -> x+1+inner) where
     inner = 3
 
 globalData = "a"
@@ -14,4 +15,10 @@ fn n = a where
     c = globalData
     t = putStrLn $ show globalData;
     a = t
-main = fn 4
+
+-- testFn::Maybe Int -> Maybe Int
+realFn::Maybe a -> Either (Maybe a) (Maybe a -> Either  (Maybe a) b)
+realFn (Just a ) = Right (realFn)
+realFn Nothing = Left $ Just 2
+-- wrapFn :: Int-> Maybe Int -> realFn
+main = putStrLn $ show $ closureFn 2
